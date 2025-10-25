6ced7fdb647f0d7ea18c0f44ccdb86cfc90d7935
immortal-p <immortalkil4oy@gmail.com>
2025-10-25 22:19:12 +0300

    fix bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import ProtectedRoute from './components/ProtenctedRoute.jsx';
-import Chat from "./components/Chat.jsx";
+import Chat from "./components/chat/Chat.jsx";
 import LogInForm from './components/Form.jsx';
 
 const App = () => {
