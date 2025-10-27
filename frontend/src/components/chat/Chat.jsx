21f9fea2c745e75be3dab78adc404c4f570f1478
immortal-p <immortalkil4oy@gmail.com>
2025-10-27 22:38:18 +0300

    feat(chat): integrate channel modals with Chat component
    - Added all modals to Chat component
    - Connected modals with Redux actions
    - Subscribed to socket events for channel updates

--- frontend/src/components/chat/Chat.jsx
+++ frontend/src/components/chat/Chat.jsx
@@ -1,20 +1,25 @@
 import React, { useEffect, useState, useRef } from "react"
 import { useSelector, useDispatch } from "react-redux"
-import { addMessage, fetchChannels, fetchMessages } from "../../slices/chatSlice"
+import { fetchChannels, fetchMessages, addMessage, addChannel, deleteChannel, renameChannel } from "../../slices/chatSlice"
 import { connectSocket } from "../../socket.js"
 import { uniqueId } from "lodash"
 import sendMessage from "./sendMessage.js"
 import "./chat.css"
-// import deleteMsg from "./deleteMsg.js"
+import { ModalAddChannel } from "./ModalAddChannel.jsx"
+import { ModalDeleteChannel } from "./ModalDeleteChannel.jsx"
+import { ModalEditChannel } from "./ModalEditChannel.jsx"
 
 const Chat = () => {
     const dispath = useDispatch()
     const { channels, messages } = useSelector((state) => state.chat)
     const [currentChannelId, setCurrentChannelId] = useState(null)
+    const [channelToDelete, setChannelToDelete] = useState(null)
+    const [channelToUpdate, setChannelToUpdate] = useState(null)
     const [newMessage, setNewMessage] = useState("")
     const [errorMsg, setErrorMSg] = useState("")
     const { username, token } = useSelector((state) => state.auth)
     const socketRef = useRef(null)
+    const defaultChannel = channels[0].id
 
     useEffect(() => {
         if(token) {
@@ -32,18 +37,28 @@ const Chat = () => {
         socket.on("newMessage", (msg) =>  {
             if(msg && msg.id) dispath(addMessage(msg))
         })
+        socket.on("newChannel", (channel) => dispath(addChannel(channel)))
+        socket.on("removeChannel", (channelId) => {
+            dispath(deleteChannel(channelId))
+            if(currentChannelId === channelId.id) {
+                setCurrentChannelId(defaultChannel)
+            }
+        })
+        socket.on("renameChannel", (channel) => dispath(renameChannel(channel)))
 
         return () => {
             socket.off("newMessage")
+            socket.off("newChannel")
+            socket.off("removeChannel")
         }
 
-    }, [dispath])
+    }, [dispath, currentChannelId, defaultChannel])
 
     useEffect(() => {
         if (channels.length > 0 && currentChannelId === null) {
-            setCurrentChannelId(channels[0].id)
+            setCurrentChannelId(defaultChannel)
         }
-    }, [channels, currentChannelId])
+    }, [channels, currentChannelId, defaultChannel])
 
     const handleChannelClick = (channelId) => {
         setCurrentChannelId(channelId)
@@ -52,16 +67,44 @@ const Chat = () => {
     const builderChannel = (channel) => {
         return (
             <li key={channel.id} className="nav-item w-100">
-                <button 
-                    type="button" 
-                    className={`w-100 rounded-0 text-start btn ${
-                        channel.id === currentChannelId ? 'btn-secondary' : ''
-                    }`}
-                    onClick={() => handleChannelClick(channel.id)}
-                >
-                    <span className="me-1">#</span>
-                    {channel.name}
-                </button>
+                {(!channel.removable) ? (
+                    <button 
+                        type="button" 
+                        className={`w-100 rounded-0 text-start btn ${
+                            channel.id === currentChannelId ? 'btn-secondary' : ''
+                        }`}
+                        onClick={() => handleChannelClick(channel.id)}
+                        >
+                        <span className="me-1">#</span>
+                        {channel.name}
+                    </button>
+                )
+                :
+                (
+                    <div role="group" className="d-flex dropdown btn-group">
+                        <button 
+                            type="button" 
+                            className={`w-100 rounded-0 text-start btn 
+                            ${channel.id === currentChannelId ? 'btn-secondary' : ''}`}
+                            onClick={() => handleChannelClick(channel.id)}
+                            >
+                            <span className="me-1">#</span>
+                            {channel.name}
+                        </button>
+                        <button 
+                            type="button" 
+                            className={`flex-grow-0 dropdown-toggle dropdown-toggle-split btn 
+                            ${channel.id === currentChannelId ? 'btn-secondary' : ''}`}
+                            data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            <span className="visually-hidden">Управление каналом</span>
+                        </button>
+                        <div className="dropdown-menu">
+                            <a className="dropdown-item" href="#" data-bs-target="#exampleModalDelete" data-bs-toggle="modal" onClick={() => setChannelToDelete(channel)}>Удалить</a>
+                            <a className="dropdown-item" href="#" data-bs-target="#exampleModalEdit" data-bs-toggle="modal" onClick={() => setChannelToUpdate(channel)}>Переименовать</a>
+                        </div>
+                    </div>
+                )}
             </li>
         )
     }
@@ -93,6 +136,17 @@ const Chat = () => {
     return (
         <div className="h-100">
             <div id="chat">
+                <ModalAddChannel
+                    onChannelCreated={(newChannel) => setCurrentChannelId(newChannel.id)} 
+                />
+                <ModalDeleteChannel
+                    channel={channelToDelete}
+                    onChannelDefault={() => setCurrentChannelId(defaultChannel)}
+                />
+                <ModalEditChannel 
+                    channel={channelToUpdate}
+                    onChannelEdited={(updatedChannel) => setChannelToUpdate(updatedChannel)}
+                    />
 
                 <div className="container h-100 my-4 overflow-hidden rounded shadow">
                     <div className="row h-100 bg-white flex-md-row">
@@ -100,7 +154,7 @@ const Chat = () => {
                         <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
                             <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
                                 <b>Каналы</b>
-                                <button type="button" className="p-0 text-primary btn btn-group-vertical">
+                                <button className="p-0 text-primary btn btn-group-vertical" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor" className="bi bi-plus-square" data-darkreader-inline-fill="">
                                         <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"></path>
                                         <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"></path>
@@ -119,7 +173,7 @@ const Chat = () => {
                             <div className="d-flex flex-column h-100">
                                 <div className="bg-light mb-4 p-3 shadow-sm small">
                                     <p className="m-0">
-                                        <b>{currentChannel ? currentChannel.name : '...'}</b>
+                                        <b># {currentChannel ? currentChannel.name : '...'}</b>
                                     </p>
                                     <span className="text-muted">{totalMessages} сообщений</span>
                                 </div>
