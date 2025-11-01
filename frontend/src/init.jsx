cbffdec7670ef73b0ca725e52ac3388d8f23fa3a
immortal-p <immortalkil4oy@gmail.com>
2025-11-02 00:16:43 +0300

    fix

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -26,7 +26,7 @@ const init = async () => {
     });
 
   const rollbarConfig = {
-    accessToken: '522190b4993341f49b425ee92846d880',
+    accessToken: '08699f4d949a48c58ec6f255539ee1d4d29fa9b1ff830183abd232712df8f05041ce466c36148261f586bc60d9d2a195',
     environment: 'testenv'
   }
 
