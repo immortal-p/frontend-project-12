cb8277639a31278700597e0e88f48765e862d180
immortal-p <immortalkil4oy@gmail.com>
2025-11-01 23:47:35 +0300

    fix bug

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,52 +1,50 @@
-import i18next from "i18next"
-import { I18nextProvider, initReactI18next} from "react-i18next"
-import App from './App.jsx'
-import resources from './locales/index.js'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import i18next from "i18next";
+import { I18nextProvider, initReactI18next } from "react-i18next";
+import App from "./App.jsx";
+import resources from "./locales/index.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import './index.css'
-import store from './slices/store.js'
-import { Provider } from 'react-redux'
-import { setupAxios } from './config/axios.js'
-import { Provider as Prov, ErrorBoundary } from "@rollbar/react"
+import './index.css';
+import store from "./slices/store.js";
+import { Provider } from "react-redux";
+import { setupAxios } from './config/axios.js';
+import { Provider as RollbarProvider, ErrorBoundary } from "@rollbar/react";
 
-const init = async () => {
-  const i18n = i18next.createInstance();
-  setupAxios()
-  
-  await i18n
-    .use(initReactI18next)
-    .init({
-      resources: resources.ru,
-      fallbackLng: 'ru',
-      interpolation: {
-        escapeValue: false,
-      }
-    });
+setupAxios();
 
+
+function TestError() {
+  throw new Error("Тестовая ошибка Rollbar");
+  return <div>Эта строка никогда не будет видна</div>;
+}
+
+export default TestError;
+
+const i18n = i18next.createInstance();
+i18n.use(initReactI18next).init({
+  resources: resources.ru,
+  fallbackLng: 'ru',
+  interpolation: { escapeValue: false }
+}).then(() => {
   const rollbarConfig = {
     accessToken: '9f7e62d53f4347d2acb395ab1cc4afc4',
     environment: 'testenv',
   };
 
-  function TestError() {
-    throw new Error("Тестовая ошибка Rollbar");
-    return <div>Эта строка никогда не будет видна</div>;
-  }
-
-  return (
-    <Prov config={rollbarConfig}>
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <RollbarProvider config={rollbarConfig}>
       <ErrorBoundary>
         <I18nextProvider i18n={i18n}>
           <Provider store={store}>
-            <App />
             <TestError />
+            <App />
           </Provider>
         </I18nextProvider>
       </ErrorBoundary>
-    </Prov>
+    </RollbarProvider>
   );
-};
-
-export default init;
+});
