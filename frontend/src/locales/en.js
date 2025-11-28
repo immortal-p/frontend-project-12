cf37d301f8cd176ea813c9c6137eeb8b6f650742
immortal-p <immortalkil4oy@gmail.com>
2025-11-28 21:02:35 +0300

    feat: add English translations

---
+++ frontend/src/locales/en.js
@@ -0,0 +1,87 @@
+export default {
+  translation: {
+    language: 'en',
+    nameChat: 'Hexlet Chat',
+    lngRu: 'Ru',
+    lngEn: 'En',
+    auth: {
+      login: {
+        title: 'Login',
+        name: 'Nickname',
+        password: 'Password',
+        button: 'Login',
+        noAccount: `Don't have account?`,
+        buttonSignUp: 'Registration',
+      },
+      register: {
+        title: 'Registration',
+        name: 'Username',
+        password: 'Password',
+        confirmPassword: 'Confirm password',
+        button: 'Registration',
+      },
+      errors: {
+        usernameLength: 'From 3 to 20 characters',
+        passwordLength: 'At least 6 characters',
+        passwordsMustMatch: 'Passwords must match',
+        confirmPasswordRequired: 'Confirm your password',
+        invalidCredentials: 'Incorrect username or password',
+        userExists: 'This user already exists',
+        connectionError: 'Connection error',
+        noToken: 'The server did not return the token.',
+      },
+    },
+
+    chat: {
+      title: 'Channels',
+      buttonExit: 'Exit',
+      messages_one: '{{count}} message',
+      messages_other: '{{count}} messages',
+      newMessage: 'New message',
+      inputMess: 'Enter message...',
+      loading: 'Loading data',
+      selectChannel: 'Select channel',
+      channelManagement: 'Channel management',
+      delete: 'Delete',
+      rename: 'Rename',
+      channelNameLabel: 'Channel name',
+      nameLabel: 'Channel name',
+      errors: {
+        connectionError: 'Failed to send message. Check your connection.',
+        unknownError: 'unknown error',
+        errorLoadingData: 'error loading data',
+      },
+      addChannelModal: {
+        title: 'Add channel',
+        confirm: 'Send',
+        cancel: 'Cancel',
+        placeholderMessage: 'Channel name',
+      },
+      editChannelModal: {
+        title: 'Rename channel',
+        confirm: 'Send',
+        cancel: 'Cancel',
+        placeholderMessage: 'New channel name',
+      },
+      deleteChannelModal: {
+        title: 'Delete channel',
+        confirmMessage: 'We are sure ?',
+        confirm: 'Delete',
+        cancel: 'Cancel',
+      },
+      toastify: {
+        loadingDataError: 'Failed to load data',
+        connectionError: 'Connection error',
+        createChannel: 'Channel created',
+        deleteChannel: 'Channel deleted',
+        renameChannel: 'Channel renamed',
+      },
+    },
+
+    errors: {
+      nameLength: 'From 3 to 20 characters',
+      existingNames: 'Must be unique',
+      required: 'Required',
+    },
+  },
+}
